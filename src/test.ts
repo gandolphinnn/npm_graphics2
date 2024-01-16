ef4915728aff49ceaba2157e4f6b018858e16b40
gandolphinnn <gandolfiluca03@gmail.com>
2024-01-16 16:13:54 +0100

    Stlye.ts problems Fixes #2 Test for the beta version

--- src/test.ts
+++ src/test.ts
@@ -8,25 +8,25 @@ import { test } from '@gandolphinnn/utils';
 //test('rgbParse HEX2', parseRGBA('#abcdef'), {red: 171, green: 205, blue: 239, alpha: 1} as RGBA);
 //test('Color.byName', Color.byName('AliceBlue'), {red: 240, green: 248, blue: 255, alpha: 1} as RGBA);
 const c1 = MainCanvas.get;
-c1.drawSampleColors();/*
+//c1.drawSampleColors();/*
 c1.drawSampleMetric(50);
 c1.drawStyle.mergeFillStyle(Color.byName('Grey'));
 c1.writeStyle.mergeTextAlign('center').mergeFont('40px arial');
 
 const centerArc	= new Arc(new Coord(274, 200), 75, new Angle(250), new Angle(0), false, true).setZ(0);
-centerArc.style.mergeFillStyle(Color.byName('Green'));
+centerArc.style.mergeFillStyle(Color.byName('SpringGreen'));
 
 const circle	= new Circle(new Coord(227.9, 200), 75).setZ(1);
 circle.style.mergeFillStyle(Color.byName('White'));
 
-const poly		= new Poly(new Coord(200, 400),new Coord(150, 200),new Coord(350, 200),new Coord(300, 400),new Coord(250, 475)).setZ(2);
-poly.style.mergeStrokeStyle(Color.byName('Brown')).mergeFillStyle(Color.byName('BurlyWood'));
+const poly		= new Poly(new Coord(215, 400),new Coord(150, 200),new Coord(350, 200),new Coord(285, 400),new Coord(250, 450)).setZ(2);
+poly.style.mergeStrokeStyle(Color.byName('SaddleBrown')).mergeLineWidth(3).mergeFillStyle(Color.byName('Sienna'));
 
-const line		= new Line(new Coord(150, 200), new Coord(300, 400)).setZ(3);
-line.style.mergeStrokeStyle(Color.byName('Brown'));
+const line		= new Line(poly.lines[0].center, poly.lines[2].center).setZ(3);
+line.style.mergeStrokeStyle(Color.byName('SaddleBrown')).mergeLineWidth(3);
 
 const arc		= new Arc(new Coord(250, 100), 141.4, new Angle(45), new Angle(135), false, false).setZ(3).setAction(RenderAction.Stroke);
-arc.style.mergeStrokeStyle(Color.byName('Brown'));
+arc.style.mergeStrokeStyle(Color.byName('SaddleBrown')).mergeLineWidth(3);
 
 const text		= new Text(new Coord(250, 300), 'TEST').setZ(9).setAction(RenderAction.None);
 const mesh		= new Mesh(new Coord(250, 400), text, line, circle, arc, centerArc, poly);
